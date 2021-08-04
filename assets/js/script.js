5fc990bdacdb233bea8ed0c8cd67705c07e35858
Rafael Escobar Salas <rafael.escobar.s@gmail.com>
2021-08-04 12:46:25 -0400

    Smooth-Scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Cambiar color de barra de navegación entre transparente y dark
 var nav = document.querySelector("nav");
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 173) {
@@ -5,4 +6,25 @@ window.addEventListener("scroll", function () {
   } else {
     nav.classList.remove("bg-dark", "shadow");
   }
+});
+
+//Hacer smooth-scroll
+$(function () {
+  $("a").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(gato).offset().top - 50,
+        },
+        800,
+        function () {
+          window.location.hash = gato;
+        }
+      );
+    }
+  });
 });
