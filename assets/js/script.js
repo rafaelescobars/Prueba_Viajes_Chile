f44cf4f7d16d1ff135b0178c8653b601cc180b1b
Rafael Escobar Salas <rafael.escobar.s@gmail.com>
2021-08-05 15:58:23 -0400

    tooltips added

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,11 @@ $(function () {
     }
   });
 });
+
+//Tooltip (footer)
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
